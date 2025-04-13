fdf435b9e382a86866f7610f30866403c952e304
GimhanDissanayake <gimhanem@gmail.com>
2025-04-13 13:01:34 +0530

    terraform vpc + pre-commit hooks

--- network.tf
+++ network.tf
@@ -25,21 +25,3 @@ module "vpc" {
 
   tags = local.tags
 }
-
-# # Ensure the default security group of every VPC restricts all traffic
-# resource "aws_default_security_group" "default" {
-#   vpc_id = module.vpc.vpc_id
-
-#   # ingress {
-#   #   protocol  = "-1"
-#   #   self      = true
-#   #   from_port = 0
-#   #   to_port   = 0
-#   # }
-#   # egress {
-#   #   from_port   = 0
-#   #   to_port     = 0
-#   #   protocol    = "-1"
-#   #   cidr_blocks = ["0.0.0.0/0"]
-#   # }
-# }
